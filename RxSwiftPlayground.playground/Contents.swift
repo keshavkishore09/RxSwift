4d51c28f1381389796d743335901b69cdaf483b6
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-14 23:01:16 +0900

    Implementing subscriptions

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -6,17 +6,41 @@ import RxSwift
 let observables = Observable.just(1)
 
 
+// Observables of int values
 let observables2 = Observable.of(1,2,3,4,5)
 
 
-
+// Obserbavles of the entire array
 let observables3 =  Observable.of([1,2,3,4,5])
 
 
-
-let observable4 = Observable.from([1,2,3,4,5,6])
-
-
+// Observables of the all single elments of the array
+let observables4 = Observable.from([1,2,3,4,5,6])
 
 
+// Need to unwrap the element
+observables4.subscribe{ event in
+    // print(event)
+    if let element = event.element{
+        print(element)
+    }
+}
 
+// Need to unwrap the element
+observables3.subscribe{event in
+    // print(event)
+    if let element = event.element {
+        print(element)
+    }
+}
+
+
+// No need to unwrap the element
+observables4.subscribe(onNext: { element in
+    print(element)
+})
+
+// No need to unwrap the element
+observables3.subscribe(onNext: { element in
+    print(element)
+})
