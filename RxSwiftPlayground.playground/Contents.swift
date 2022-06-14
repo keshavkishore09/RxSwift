e7a9b5a9c0aee1e6cc4b4a50c450795ecd190555
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-14 21:26:00 +0900

    Implementing Observables

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -3,6 +3,20 @@ import RxSwift
 
 
 
-_ = Observable.from([1,2,3,4,5])
+let observables = Observable.just(1)
+
+
+let observables2 = Observable.of(1,2,3,4,5)
+
+
+
+let observables3 =  Observable.of([1,2,3,4,5])
+
+
+
+let observable4 = Observable.from([1,2,3,4,5,6])
+
+
+
 
 
