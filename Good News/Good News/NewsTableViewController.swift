e53a9a4b41e6baf7c27a3d8731a829c7712cb6d2
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-22 23:16:25 +0900

    User interface good news app

---
+++ Good News/Good News/NewsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsTableViewController.swift
+//  Good News
+//
+//  Created by Keshav Kishore on 22/06/22.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+    
+    
+}
