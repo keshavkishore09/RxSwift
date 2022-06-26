cbb8047d6c7cab05a47dba800f2082a18821491c
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-26 21:16:18 +0900

    Setting up the UI for News app in MVVM

---
+++ NewsAppMVVM/NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsTableViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Keshav Kishore on 26/06/22.
+//
+
+import UIKit
+
+
+class NewsTableVieController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
