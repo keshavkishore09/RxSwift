3ebdaf10f0fdbafbaa6a73ae2c3148f6143d984c
Keshav Kishore <keshavkishore09@gmail.com>
2022-06-26 20:19:56 +0900

    Throe Rxcocoa error

--- GoodWeather/GoodWeather/Extensions/URLRequest+Extension.swift
+++ GoodWeather/GoodWeather/Extensions/URLRequest+Extension.swift
@@ -19,6 +19,23 @@ struct Resource<T> {
 
 
 extension URLRequest {
+    
+    
+    static func load<T: Decodable>(ressource: Resource<T>) ->  Observable<T> {
+        return Observable.just(ressource.url).flatMap { url -> Observable<(response : HTTPURLResponse, data: Data)> in
+            let request = URLRequest(url: url)
+            return  URLSession.shared.rx.response(request:  request)
+        }.map { response, data  -> T in
+            if 200..<300 ~= response.statusCode {
+                return try JSONDecoder().decode(T.self, from: data)
+            } else {
+                throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+            }
+        }.asObservable()
+    }
+    
+    
+    /*
     static func load<T: Decodable>(resource: Resource<T>) -> Observable<T?> {
         return Observable.from([resource.url]).flatMap { url -> Observable<Data> in
              let request = URLRequest(url: url)
@@ -32,5 +49,7 @@ extension URLRequest {
             }
         }.asObservable()
     }
+
+     */
 }
  
